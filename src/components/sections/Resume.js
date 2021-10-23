bb2dc5257bbb5f6722f08fd2cd0594252c8075f5
HPainhas <hpainhas@hotmail.com>
2021-10-23 12:13:20 -0400

    Started implementing the resume section.

--- src/components/sections/Resume.js
+++ src/components/sections/Resume.js
@@ -5,7 +5,18 @@ const Resume = () => {
     return (
         <Fragment>
             {resumeData !== null ? (
-                <section id='resume' className='section-container'></section>
+                <section id='resume' className='section-container'>
+                    <div className='resume'>
+                        <div className='container'>
+                            <div className='position-relative'>
+                                <div className='title'>
+                                    <span>Resume</span>
+                                    <h3>My Background</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             ) : null}
         </Fragment>
     );
