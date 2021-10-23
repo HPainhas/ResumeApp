8e46347fccb9215fee30c8f21c2b84467ab69e7e
HPainhas <hpainhas@hotmail.com>
2021-10-22 22:29:50 -0400

    Done with the About section.

--- src/components/sections/Portfolio.js
+++ src/components/sections/Portfolio.js
@@ -2,17 +2,13 @@ import React, { Fragment } from 'react';
 import resumeData from '../../assets/resume.json';
 
 const Portfolio = () => {
-	return (
-		<Fragment>
-			{resumeData !== null ? (
-				<section id='portfolio' className='portfolio'>
-					<div className='portfolio-inner'>
-						<h1>Portfolio Page</h1>
-					</div>
-				</section>
-			) : null}
-		</Fragment>
-	);
+    return (
+        <Fragment>
+            {resumeData !== null ? (
+                <section id='portfolio' className='section-container'></section>
+            ) : null}
+        </Fragment>
+    );
 };
 
 export default Portfolio;
--- src/components/sections/Resume.js
+++ src/components/sections/Resume.js
@@ -2,17 +2,13 @@ import React, { Fragment } from 'react';
 import resumeData from '../../assets/resume.json';
 
 const Resume = () => {
-	return (
-		<Fragment>
-			{resumeData !== null ? (
-				<section id='resume' className='resume'>
-					<div className='resume-inner'>
-						<h1>Resume Page</h1>
-					</div>
-				</section>
-			) : null}
-		</Fragment>
-	);
+    return (
+        <Fragment>
+            {resumeData !== null ? (
+                <section id='resume' className='section-container'></section>
+            ) : null}
+        </Fragment>
+    );
 };
 
 export default Resume;
