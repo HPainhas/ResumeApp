0a87b48e453b55450d9865c060ddd496c046e18c
henrique-painhas <hpainhas@gmail.com>
2022-12-14 17:32:52 -0700

    Ran prettier plus set up github pages

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,56 +1,60 @@
-import React, { Fragment, useEffect } from 'react';
-import logo from '../../assets/HPainhas_logo_medium.png';
-import $ from 'jquery';
+import React, { Fragment, useEffect } from "react";
+import logo from "../../assets/HPainhas_logo_medium.png";
+import $ from "jquery";
 
 const Navbar = () => {
-    useEffect(() => {
-        $(function () {
-            $(window).on('scroll', function () {
-                if ($(window).scrollTop() > 200) {
-                    $('.navbar-inner').addClass('opened-dark');
-                } else {
-                    $('.navbar-inner').removeClass('opened-dark');
-                }
-            });
-        });
+	useEffect(() => {
+		$(function () {
+			$(window).on("scroll", function () {
+				if ($(window).scrollTop() > 200) {
+					$(".navbar-inner").addClass("opened-dark");
+				} else {
+					$(".navbar-inner").removeClass("opened-dark");
+				}
+			});
+		});
 
-        $('#anchor-nav > li').on('click', function () {
-            $(this).addClass('current').siblings().removeClass('current');
-        });
-    }, []);
+		$("#anchor-nav > li").on("click", function () {
+			$(this).addClass("current").siblings().removeClass("current");
+		});
+	}, []);
 
-    return (
-        <Fragment>
-            <div className='navbar'>
-                <div className='navbar-inner'>
-                    <div className='logo'>
-                        <a aria-current='page' className='active' href='/'>
-                            <img src={logo} alt='HPainhas Logo' />
-                        </a>
-                    </div>
-                    <div className='menu'>
-                        <ul id='anchor-nav' className='anchor-nav'>
-                            <li className='current'>
-                                <a href='#home'>Home</a>
-                            </li>
-                            <li>
-                                <a href='#about'>About</a>
-                            </li>
-                            <li>
-                                <a href='#portfolio'>Portfolio</a>
-                            </li>
-                            <li>
-                                <a href='#resume'>Resume</a>
-                            </li>
-                            <li>
-                                <a href='#contact'>Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			<div className="navbar">
+				<div className="navbar-inner">
+					<div className="logo">
+						<a
+							aria-current="page"
+							className="active"
+							href="https://hpainhas.github.io/resume-app/"
+						>
+							<img src={logo} alt="HPainhas Logo" />
+						</a>
+					</div>
+					<div className="menu">
+						<ul id="anchor-nav" className="anchor-nav">
+							<li className="current">
+								<a href="#home">Home</a>
+							</li>
+							<li>
+								<a href="#about">About</a>
+							</li>
+							<li>
+								<a href="#portfolio">Portfolio</a>
+							</li>
+							<li>
+								<a href="#resume">Resume</a>
+							</li>
+							<li>
+								<a href="#contact">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
 };
 
 export default Navbar;
--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,100 +1,98 @@
-import React, { Fragment } from 'react';
-import SimpleModal from '../layout/SimpleModal';
-import resumeData from '../../assets/resume.json';
-import ColumbusPic from '../../assets/belo_horizonte_skyline.jpg';
-import { SiGithub } from 'react-icons/si';
-import { FaLinkedin } from 'react-icons/fa';
-import { FiPhoneCall } from 'react-icons/fi';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BiCurrentLocation } from 'react-icons/bi';
+import React, { Fragment } from "react";
+import SimpleModal from "../layout/SimpleModal";
+import resumeData from "../../assets/resume.json";
+import ColumbusPic from "../../assets/belo_horizonte_skyline.jpg";
+import { SiGithub } from "react-icons/si";
+import { FaLinkedin } from "react-icons/fa";
+import { BiCurrentLocation } from "react-icons/bi";
 
 const contact = resumeData.contact;
 
 const Contact = () => {
-    const contactDisplay = contact.map(info => {
-        let icon = null;
+	const contactDisplay = contact.map((info) => {
+		let icon = null;
 
-        switch (info.type) {
-            case 'location':
-                icon = (
-                    <BiCurrentLocation
-                        className='svg'
-                        size='2rem'
-                        color={'white'}
-                    />
-                );
-                break;
-            case 'linkedin':
-                icon = (
-                    <a href={info.url} target='_blank' rel='noreferrer'>
-                        <FaLinkedin
-                            className='svg'
-                            size='2rem'
-                            color={'white'}
-                        />
-                    </a>
-                );
-                break;
-            case 'github':
-                icon = (
-                    <a href={info.url} target='_blank' rel='noreferrer'>
-                        <SiGithub className='svg' size='2rem' color={'white'} />
-                    </a>
-                );
-                break;
-            default:
-                icon = null;
-                break;
-        }
+		switch (info.type) {
+			case "location":
+				icon = (
+					<BiCurrentLocation
+						className="svg"
+						size="2rem"
+						color={"white"}
+					/>
+				);
+				break;
+			case "linkedin":
+				icon = (
+					<a href={info.url}>
+						<FaLinkedin
+							className="svg"
+							size="2rem"
+							color={"white"}
+						/>
+					</a>
+				);
+				break;
+			case "github":
+				icon = (
+					<a href={info.url}>
+						<SiGithub className="svg" size="2rem" color={"white"} />
+					</a>
+				);
+				break;
+			default:
+				icon = null;
+				break;
+		}
 
-        return (
-            <li key={info.value}>
-                <div className='contact-info-inner'>
-                    {icon}
-                    <p>{info.value}</p>
-                </div>
-            </li>
-        );
-    });
+		return (
+			<li key={info.value}>
+				<div className="contact-info-inner">
+					{icon}
+					<p>{info.value}</p>
+				</div>
+			</li>
+		);
+	});
 
-    return (
-        <Fragment>
-            {contact !== null ? (
-                <section id='contact' className='section-container'>
-                    <div
-                        className='contact big-image'
-                        style={{
-                            backgroundImage: `url(${ColumbusPic})`,
-                        }}
-                    >
-                        <div className='shape'>
-                            <img
-                                className='svg'
-                                src='http://shanereact.ibthemespro.com/img/svg/paper.svg'
-                                alt='paper shape'
-                            />
-                        </div>
-                        <div id='talk' className='background'></div>
-                        <div className='contact-inner'>
-                            <div className='text' data-aos='zoom-in'>
-                                <h3>Let's connect!</h3>
-                            </div>
-                            <div data-aos='fade-up'>
-                                <SimpleModal buttonLabel='Contact Me'>
-                                    <div className='contact-modal'>
-                                        <h2>Get in touch</h2>
-                                        <div className='contact-info'>
-                                            <ul>{contactDisplay}</ul>
-                                        </div>
-                                    </div>
-                                </SimpleModal>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            ) : null}
-        </Fragment>
-    );
+	return (
+		<Fragment>
+			{contact !== null ? (
+				<section id="contact" className="section-container">
+					<div
+						className="contact big-image"
+						style={{
+							backgroundImage: `url(${ColumbusPic})`,
+						}}
+					>
+						<div className="shape">
+							<img
+								className="svg"
+								src="http://shanereact.ibthemespro.com/img/svg/paper.svg"
+								alt="paper shape"
+							/>
+						</div>
+						<div id="talk" className="background"></div>
+						<div className="contact-inner">
+							<div className="text" data-aos="zoom-in">
+								<h3>Let's connect!</h3>
+							</div>
+							<div data-aos="fade-up">
+								<SimpleModal buttonLabel="Contact Me">
+									<div className="contact-modal">
+										<h2>Get in touch</h2>
+										<div className="contact-info">
+											<ul>{contactDisplay}</ul>
+										</div>
+									</div>
+								</SimpleModal>
+							</div>
+						</div>
+					</div>
+				</section>
+			) : null}
+		</Fragment>
+	);
 };
 
 export default Contact;
