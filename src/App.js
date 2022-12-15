2ada36a08eba6a346b7507debfc65e6af7c3dd71
henrique-painhas <hpainhas@gmail.com>
2022-12-14 17:41:43 -0700

    change default path

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import resumeData from './assets/resume.json';
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/pages/Landing';
-import NotFound from './components/layout/NotFound';
-import MobileNavbar from './components/layout/MobileNavbar';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import resumeData from "./assets/resume.json";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/pages/Landing";
+import NotFound from "./components/layout/NotFound";
+import MobileNavbar from "./components/layout/MobileNavbar";
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import './App.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./App.css";
 
 const App = () => {
-    AOS.init({
-        delay: 200,
-        duration: 1000,
-        once: false,
-        mirror: true,
-    });
-    AOS.refresh();
+	AOS.init({
+		delay: 200,
+		duration: 1000,
+		once: false,
+		mirror: true,
+	});
+	AOS.refresh();
 
-    return (
-        <Router>
-            <Fragment>
-                <Navbar />
-                <MobileNavbar />
-                <Switch>
-                    <Route exact path='/' component={Landing} />
-                    <Route component={NotFound} />
-                </Switch>
-                <Footer resumeData={resumeData} />
-            </Fragment>
-        </Router>
-    );
+	return (
+		<Router>
+			<Fragment>
+				<Navbar />
+				<MobileNavbar />
+				<Switch>
+					<Route
+						exact
+						path="https://hpainhas.github.io/resume-app/"
+						component={Landing}
+					/>
+					<Route component={NotFound} />
+				</Switch>
+				<Footer resumeData={resumeData} />
+			</Fragment>
+		</Router>
+	);
 };
 
 export default App;
