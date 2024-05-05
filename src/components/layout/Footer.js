847d879d7bc5b2967e3b77a1f581aa37b2f5d4e6
henrique-painhas <hpainhas@gmail.com>
2024-05-05 13:12:46 -0700

    Removed react-fade-in and update navbar css

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react";
-import FadeIn from "react-fade-in";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/fontawesome-free-solid";
 
@@ -7,26 +6,22 @@ const Footer = () => {
 	return (
 		<Fragment>
 			<footer>
-				<FadeIn bottom>
-					<section id="footer" className="section-container">
-						<div className="scroll-up">
-							<a className="smooth-scroll" href="#home">
-								<FontAwesomeIcon icon={faChevronUp} size="2x" />
-							</a>
+				<section id="footer" className="section-container">
+					<div className="scroll-up">
+						<a className="smooth-scroll" href="#home">
+							<FontAwesomeIcon icon={faChevronUp} size="2x" />
+						</a>
+					</div>
+					<div className="social-container">
+						<div className="top-10">
+							<span className="barely-readable">
+								&copy; 2023 by{" "}
+							</span>
+							<span className="full-name">Henrique Painhas</span>.
+							All rights reserved.
 						</div>
-						<div className="social-container">
-							<div className="top-10">
-								<span className="barely-readable">
-									&copy; 2023 by{" "}
-								</span>
-								<span className="full-name">
-									Henrique Painhas
-								</span>
-								. All rights reserved.
-							</div>
-						</div>
-					</section>
-				</FadeIn>
+					</div>
+				</section>
 			</footer>
 		</Fragment>
 	);
